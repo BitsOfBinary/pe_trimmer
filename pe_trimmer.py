a7a984fbfa6491e841978d0f784968ccfc9c9cac
BitsOfBinary <48534004+BitsOfBinary@users.noreply.github.com>
2020-05-28 22:31:21 +0100

    Code reformatting

--- pe_trimmer.py
+++ pe_trimmer.py
@@ -1,5 +1,6 @@
 import struct
 import sys
+import argparse
 
 
 class ParsedPEHeader:
@@ -20,43 +21,67 @@ class ParsedPEHeader:
 
     def calculate_pe_header_offset(self):
         # 0x3c in is the PE header offset
-        self.pe_header_offset = struct.unpack("I", self.pe_data[0x3c: 0x3c + 4])[0]
+        self.pe_header_offset = struct.unpack("I", self.pe_data[0x3C : 0x3C + 4])[0]
 
     def calculate_checksum_offset(self):
         # 0x58 offset into the PE header is the CheckSum value
         self.checksum_offset = self.pe_header_offset + 0x58
 
     def calculate_checksum(self):
-        self.pe_checksum = struct.unpack("I", self.pe_data[self.checksum_offset: self.checksum_offset + 4])[0]
+        self.pe_checksum = struct.unpack(
+            "I", self.pe_data[self.checksum_offset : self.checksum_offset + 4]
+        )[0]
 
     def calculate_optional_header_offset(self):
         self.optional_header_offset = self.pe_header_offset + 24
 
     def calculate_optional_header_size(self):
         size_of_optional_header_offset = self.pe_header_offset + 20
-        self.optional_header_size = \
-            struct.unpack("H", self.pe_data[size_of_optional_header_offset: size_of_optional_header_offset + 2])[0]
+        self.optional_header_size = struct.unpack(
+            "H",
+            self.pe_data[
+                size_of_optional_header_offset : size_of_optional_header_offset + 2
+            ],
+        )[0]
 
     def calculate_section_table_offset(self):
-        self.section_table_offset = self.optional_header_offset + self.optional_header_size
+        self.section_table_offset = (
+            self.optional_header_offset + self.optional_header_size
+        )
 
     def calculate_number_of_sections(self):
         self.number_of_sections = self.pe_data[self.pe_header_offset + 6]
 
     def calculate_final_section_table_entry_offset(self):
         # Each section table entry is 40 bytes
-        self.final_section_table_entry_offset = self.section_table_offset + (self.number_of_sections - 1) * 40
+        self.final_section_table_entry_offset = (
+            self.section_table_offset + (self.number_of_sections - 1) * 40
+        )
 
     def calculate_final_section_offset(self):
-        self.final_section_offset = struct.unpack("I", self.pe_data[self.final_section_table_entry_offset + 20:
-                                                                    self.final_section_table_entry_offset + 24])[0]
+        self.final_section_offset = struct.unpack(
+            "I",
+            self.pe_data[
+                self.final_section_table_entry_offset
+                + 20 : self.final_section_table_entry_offset
+                + 24
+            ],
+        )[0]
 
     def calculate_final_section_raw_size(self):
-        self.final_section_raw_size = struct.unpack("I", self.pe_data[self.final_section_table_entry_offset + 16:
-                                                                      self.final_section_table_entry_offset + 20])[0]
+        self.final_section_raw_size = struct.unpack(
+            "I",
+            self.pe_data[
+                self.final_section_table_entry_offset
+                + 16 : self.final_section_table_entry_offset
+                + 20
+            ],
+        )[0]
 
     def calculate_end_of_final_section_offset(self):
-        self.end_of_final_section_offset = self.final_section_offset + self.final_section_raw_size
+        self.end_of_final_section_offset = (
+            self.final_section_offset + self.final_section_raw_size
+        )
 
     def get_checksum_offset(self):
         return self.checksum_offset
@@ -103,15 +128,15 @@ class PETrimmer:
             if i == int(self.checksum_offset / 4):
                 continue
 
-            dword = struct.unpack("I", self.pe_data[i * 4: (i * 4) + 4])[0]
-            checksum = (checksum & 0xffffffff) + dword + (checksum >> 32)
+            dword = struct.unpack("I", self.pe_data[i * 4 : (i * 4) + 4])[0]
+            checksum = (checksum & 0xFFFFFFFF) + dword + (checksum >> 32)
 
             if checksum > top:
-                checksum = (checksum & 0xffffffff) + (checksum >> 32)
+                checksum = (checksum & 0xFFFFFFFF) + (checksum >> 32)
 
-        checksum = (checksum & 0xffff) + (checksum >> 16)
+        checksum = (checksum & 0xFFFF) + (checksum >> 16)
         checksum = checksum + (checksum >> 16)
-        checksum = checksum & 0xffff
+        checksum = checksum & 0xFFFF
 
         checksum += len(self.pe_data)
 
@@ -130,7 +155,7 @@ class PETrimmer:
         self.checksum_offset = parsed_pe_header.get_checksum_offset()
 
     def trim_pe_data(self):
-        del (self.pe_data[-1])
+        del self.pe_data[-1]
 
     def get_true_pe_checksum(self):
         return self.true_pe_checksum
@@ -145,50 +170,63 @@ class PETrimmer:
         return self.pe_data
 
 
-if len(sys.argv) != 3:
-    print("Usage: python pe_trimmer.py <PE_FILE> <OUTPUT_FILE>")
-    sys.exit()
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python pe_trimmer.py <PE_FILE> <OUTPUT_FILE>")
+        sys.exit()
 
-input_file_path = sys.argv[1]
-output_file_path = sys.argv[2]
+    input_file_path = sys.argv[1]
+    output_file_path = sys.argv[2]
 
-pe_trimmer = PETrimmer()
-pe_trimmer.load_pe_data(input_file_path)
-pe_trimmer.parse_pe_header()
-pe_trimmer.calculate_pe_checksum()
+    pe_trimmer = PETrimmer()
+    pe_trimmer.load_pe_data(input_file_path)
+    pe_trimmer.parse_pe_header()
+    pe_trimmer.calculate_pe_checksum()
 
-print("PE CheckSum from header: %s" % (hex(pe_trimmer.get_true_pe_checksum())))
-print("Calculated PE CheckSum: %s\n" % (hex(pe_trimmer.get_calculated_pe_checksum())))
+    print("PE CheckSum from header: %s" % (hex(pe_trimmer.get_true_pe_checksum())))
+    print(
+        "Calculated PE CheckSum: %s\n" % (hex(pe_trimmer.get_calculated_pe_checksum()))
+    )
 
-if pe_trimmer.get_true_pe_checksum() == pe_trimmer.get_calculated_pe_checksum():
-    print("The CheckSum of %s is already correct." % input_file_path)
+    if pe_trimmer.get_true_pe_checksum() == pe_trimmer.get_calculated_pe_checksum():
+        print("The CheckSum of %s is already correct." % input_file_path)
 
-else:
-    print("The CheckSum of %s does not match the calculated CheckSum.\n" % input_file_path)
-    print('Overlay offset: %s' % (hex(pe_trimmer.get_overlay_offset())))
+    else:
+        print(
+            "The CheckSum of %s does not match the calculated CheckSum.\n"
+            % input_file_path
+        )
+        print("Overlay offset: %s" % (hex(pe_trimmer.get_overlay_offset())))
 
-    max_steps = len(pe_trimmer.get_pe_data()) - pe_trimmer.get_overlay_offset()
+        max_steps = len(pe_trimmer.get_pe_data()) - pe_trimmer.get_overlay_offset()
 
-    print('Max iterations to take: %d\n' % max_steps)
-    print("Beginning to remove bytes...\n")
+        print("Max iterations to take: %d\n" % max_steps)
+        print("Beginning to remove bytes...\n")
 
-    for i in range(1, max_steps):
-        pe_trimmer.trim_pe_data()
+        for i in range(1, max_steps):
+            pe_trimmer.trim_pe_data()
 
-        pe_trimmer.calculate_pe_checksum()
+            pe_trimmer.calculate_pe_checksum()
 
-        if pe_trimmer.get_true_pe_checksum() == pe_trimmer.get_calculated_pe_checksum():
-            iterations_done = i
-            break
+            if (
+                pe_trimmer.get_true_pe_checksum()
+                == pe_trimmer.get_calculated_pe_checksum()
+            ):
+                iterations_done = i
+                break
 
-        if i == max_steps:
-            print("Max iterations reached, CheckSums don't match.\n")
-            sys.exit()
+            if i == max_steps:
+                print("Max iterations reached, CheckSums don't match.\n")
+                sys.exit()
 
-    print("CheckSums match!")
-    print("Iterations taken: %d\n" % i)
+        print("CheckSums match!")
+        print("Iterations taken: %d\n" % i)
 
-    with open(output_file_path, "wb") as outfile:
-        outfile.write(pe_trimmer.get_pe_data())
+        with open(output_file_path, "wb") as outfile:
+            outfile.write(pe_trimmer.get_pe_data())
 
-    print('Saved output binary to: %s' % output_file_path)
+        print("Saved output binary to: %s" % output_file_path)
+
+
+if __name__ == "__main__":
+    main()
